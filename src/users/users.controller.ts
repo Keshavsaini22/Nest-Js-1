ae23d5f1392eeb6a844eea0a957dfd76ec2d23d6
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-07-18 17:15:40 +0530

    service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,47 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') //localhost:3000/users
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService) { }
+
     @Get()    //GET /users  or /users?role=admin
-    findAll(@Query('role') role?: 'admin' | 'intern') {
-        if (role) {
-            return { role: role };
-        }
-        return [];
+    findAll(@Query('role') role?: 'Standard' | 'Basic' | 'Premium') {
+        // if (role) {
+        //     return { role: role };
+        // }
+        // return [];
+        return this.usersService.findAll(role);
     }
 
-    @Get('intern')  // GET /users/intern
-    findAllIntern() {
-        return { name: 'Intern' };
-    }
+    // @Get('intern')  // GET /users/intern
+    // findAllIntern() {
+    //     return { name: 'Intern' };
+    // }
 
     @Get(':id')    //GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id };
+        // return { id };
+        return this.usersService.findOne(id);
     }
 
     @Post()    //POST /users
-    create(@Body() user: {}) {
-        return user;
+    create(@Body() user: any) {
+        // return user;
+        return this.usersService.create(user);
     }
 
     @Patch(':id')    //PATCH /users/:id
     update(@Param('id') id: string, @Body() user: {}) {
-        return { id, ...user };
+        // return { id, ...user };
+        return this.usersService.update(id, user);
     }
 
     @Delete(':id')    //DELETE /users/:id
     remove(@Param('id') id: string) {
-        return { id };
+        // return { id };
+        return this.usersService.delete(id);
     }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,74 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "name": "Alice",
+            "age": 30,
+            "email": "alice@example.com",
+            "country": "USA",
+            "subscription": "Premium"
+        },
+        {
+            "name": "Bob",
+            "age": 25,
+            "email": "bob@example.com",
+            "country": "Canada",
+            "subscription": "Basic"
+        },
+        {
+            "name": "Charlie",
+            "age": 35,
+            "email": "charlie@example.com",
+            "country": "UK",
+            "subscription": "Standard"
+        },
+        {
+            "name": "Diana",
+            "age": 28,
+            "email": "diana@example.com",
+            "country": "Australia",
+            "subscription": "Premium"
+        },
+        {
+            "name": "Eve",
+            "age": 22,
+            "email": "eve@example.com",
+            "country": "New Zealand",
+            "subscription": "Basic"
+        }
+    ]
+
+    findAll(role?: 'Standard' | 'Basic' | 'Premium') {
+        if (role) {
+            return this.users.filter(user => user.subscription === role);
+        }
+        return this.users;
+    }
+
+    findOne(id: string) {
+        return this.users.find(user => user.age === Number(id));
+    }
+
+    create(user: { name: string, age: number, email: string, country: string, subscription: string }) {
+        this.users.push(user);
+        return user;
+    }
+
+    update(id: string, updatedUser: { name?: string, age?: number, email?: string, country?: string, subscription?: string }) {
+        this.users = this.users.map(user => {
+            if (user.age === Number(id)) {
+                return { ...user, ...updatedUser }
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+
+    delete(id: string) {
+        this.users = this.users.filter(user => user.age !== Number(id));
+        return { deleted: true }
+    }
+    
+}
